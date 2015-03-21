512ec1d1abafd6229d5184987ef3dc8255c90c7e
Viraat Aryabumi Reddy <viraat94@gmail.com>
2015-03-21 23:35:35 +0530

    ping

---
+++ ping/implementation.java
@@ -0,0 +1,30 @@
+// Implementation of ping service
+import java.io.*;
+import java.util.*;
+class pingTest {
+
+    public static void main(String[] args) {
+
+        String ip = "192.168.1.100";
+        String pingResult = "";
+
+        String pingCmd = "ping " + ip;
+        try {
+            Runtime r = Runtime.getRuntime();
+            Process p = r.exec(pingCmd);
+
+            BufferedReader in = new BufferedReader(new
+            InputStreamReader(p.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+                pingResult += inputLine;
+            }
+            in.close();
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+    }
+}
