20d344d2583ab48396d832b54adfbbed866ba299
Viraat Aryabumi Reddy <viraat94@gmail.com>
2015-04-15 15:14:38 +0530

    Create nonblockingio.c

---
+++ nonblocking-io/nonblockingio.c
@@ -0,0 +1,29 @@
+//Program
+#include <fcntl.h>
+#include <errno.h>
+#include<stdio.h>
+#define BUFFSIZE 100
+main()
+{
+  int n;
+  char buff[BUFFSIZE]; 
+  if(fcntl(0,F_SETFL,FNDELAY)<0)
+  {
+    perror("Nonblocking failed");
+    exit(0);
+  }
+  while(1)
+  {
+    while((n=read(0,buff,BUFFSIZE))<0)
+    {
+      if(errno!=EWOULDBLOCK)
+        break;
+    }
+    fflush(stdout);
+    if(write(1,buff,n)<0)
+    {
+      perror("write");
+      break;
+    }
+  }
+} 
